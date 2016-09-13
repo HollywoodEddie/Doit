af67917eda7d56b3cd610af8e5555866fce6ae22
MAC <mac@172-13-8-100.lightspeed.wchtks.sbcglobal.net>
2016-09-13 13:35:19 +0800

    Created classes and first table view

--- Doit/ViewController.swift
+++ Doit/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+       
+        tasks = makeTask()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        let task = tasks [indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️\(task.name)"
 
-}
+        }else {
+        cell.textLabel?.text = task.name
+        }
+            return cell
+        
+    }
+    func makeTask() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the Dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Get Cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Go to Gym"
+        task3.important = false
+        
+        return [ task1, task2, task3]
+        
+    }
+   
+    }
+
+
 
---
+++ Doit/task.swift
@@ -0,0 +1,15 @@
+//
+//  task.swift
+//  Doit
+//
+//  Created by MAC on 9/13/16.
+//  Copyright © 2016 MAC. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+
+}
