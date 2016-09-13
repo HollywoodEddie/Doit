1d7f105bc1b8d4f1609f35360242b505cf7ae0b5
MAC <mac@172-13-8-100.lightspeed.wchtks.sbcglobal.net>
2016-09-13 14:10:08 +0800

    Added Labels, buttons and connected the CreateTask tableview

---
+++ Doit/CreateTaskViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by MAC on 9/13/16.
+//  Copyright © 2016 MAC. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var ImportantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(sender: AnyObject) {
+        
+    }
+    
+  
+
+ 
+
+}
